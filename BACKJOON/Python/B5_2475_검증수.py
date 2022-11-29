667d36d390b7c97d564c41bbd36c000abc61e2bd
Im-Hass <huiseon4107@gmail.com>
2022-11-30 00:11:47 +0900

    [Python] Baekjoon B5

---
+++ BACKJOON/Python/B5_2475_검증수.py
@@ -0,0 +1,2 @@
+nums = list(map(lambda x: int(x)**2, input().split(' ')))
+print(sum(nums) % 10)
---
+++ BACKJOON/Python/B5_2744_대소문자바꾸기.py
@@ -0,0 +1,5 @@
+print(''.join(list(map(lambda x: x.lower() if x.isupper() else x.upper(), list(input())))))
+
+# swapcase() 메서드를 사용하면 한 번에 변환 가능
+# a = input()
+# print(a.swapcase())
