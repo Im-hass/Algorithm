9075585ab6c13d3413944e715e0bfe730d1f933e
Im-hass <huiseon4107@gmail.com>
2024-04-30 23:33:13 +0900

    [BOJ] G4_1043 거짓말 - 리팩토링

--- BACKJOON/Java/Main_G4_1043_거짓말.java
+++ BACKJOON/Java/Main_G4_1043_거짓말.java
@@ -8,7 +8,6 @@ public class Main_G4_1043_거짓말 {
   static int[] parent; // 모든 사람의 루트
 
   public static void main(String[] args) throws IOException {
-
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(in.readLine());
 
@@ -29,20 +28,28 @@ public class Main_G4_1043_거짓말 {
 
     int[] partyRoots = new int[M]; // 각 파티의 루트
     for (int i = 0; i < M; i++) {
-      st = new StringTokenizer(in.readLine());
-      int cnt = Integer.parseInt(st.nextToken());
-      int a = Integer.parseInt(st.nextToken());
-      partyRoots[i] = a;
-      for (int j = 0; j < cnt - 1; j++) {
-        int b = Integer.parseInt(st.nextToken());
-        union(a, b);
-      }
+      partyRoots[i] = setParty(in);
     }
 
-    int answer = M; // 거짓말할 수 있는 파티의 수
+    int answer = countLies(truth, partyRoots, truthRoots);
+    System.out.println(answer);
+  }
+
+  public static int setParty(BufferedReader in) throws IOException {
+    StringTokenizer st = new StringTokenizer(in.readLine());
+    int cnt = Integer.parseInt(st.nextToken());
+    int a = Integer.parseInt(st.nextToken());
+    for (int j = 0; j < cnt - 1; j++) {
+      int b = Integer.parseInt(st.nextToken());
+      union(a, b);
+    }
+    return a;
+  }
+
+  public static int countLies(int truth, int[] partyRoots, int[] truthRoots) {
+    int answer = partyRoots.length; // 거짓말할 수 있는 파티의 수
     if (truth == 0) { // 진실을 아는 사람이 0명이라면 그대로 return
-      System.out.println(answer);
-      return;
+      return answer;
     }
 
     for (int i = 0; i < partyRoots.length; i++) { // union이 끝난 후 각 파티의 루트의 루트 찾기
@@ -61,9 +68,7 @@ public class Main_G4_1043_거짓말 {
         }
       }
     }
-
-    System.out.println(answer);
-
+    return answer;
   }
 
   public static int find(int a) {
@@ -79,7 +84,5 @@ public class Main_G4_1043_거짓말 {
     if (aRoot == bRoot)
       return;
     parent[bRoot] = aRoot;
-    return;
   }
-
 }
