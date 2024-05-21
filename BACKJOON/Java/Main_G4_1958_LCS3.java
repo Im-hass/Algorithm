77182cb159ee1ab8e9520fa4f999c07c83562f83
Im-hass <huiseon4107@gmail.com>
2024-05-21 20:30:52 +0900

    파일명 수정 G4_1958_LCS3 -> Main_G4_1958_LCS3

--- BACKJOON/Java/Main_G4_1958_LCS3.java
+++ BACKJOON/Java/Main_G4_1958_LCS3.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class G4_1958_LCS3 {
+public class Main_G4_1958_LCS3 {
 
   public static void main(String[] args) throws IOException {
 
